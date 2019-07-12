cfe345d6beb10cba05de32a22f59df2aa7819412
Boots2291 <buteramv@gmail.com>
2019-07-12 11:14:04 -0400

    setup user creation, authentication, and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Account Creation
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+  # Login and Logout functionality
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
